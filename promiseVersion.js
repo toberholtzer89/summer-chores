35ed5d1f19dae3d1565be116018e464855221cef
Travis <travis.p.oberholtzer@gmail.com>
2025-10-27 22:40:07 -0400

    Final commit with corrections made

--- promiseVersion.js
+++ promiseVersion.js
@@ -1,78 +1,70 @@
-const mowYard = new Promise((resolve, reject) => {
-  setTimeout((name) => {
-    resolve(`${name} mowed the yard.`);
-  }, 2000);
-});
+function mowYard(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`${name} mowed the yard.`);
+    }, 2000);
+  });
+}
 
+function weedEat(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() <= 0.88) {
+        resolve(`${name} finished using the weed eater.`);
+      } else {
+        reject(`${name} fell asleep after mowing the yard.`);
+      }
+    }, 1500);
+  });
+}
 
-const weedEat = new Promise((resolve, reject) => {
-  setTimeout((name) => {
-    if (Math.random() <= 0.8) {
-      resolve(`${name} finished using the weed eater.`);
-    } else {
-      reject(`${name} fell asleep after mowing the yard.`);
-    }
-  }, 1500);
-});
+function trimHedges(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() <= 0.82) {
+        resolve(`${name} finished trimming the hedges.`);
+      } else {
+        reject(`${name} fell asleep after weed eating the yard.`);
+      }
+    }, 1000);
+  });
+}
 
+function collectWood(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() <= 0.75) {
+        resolve(`${name} finished collecting wood.`);
+      } else {
+        reject(`${name} fell asleep after trimming the hedges.`);
+      }
+    }, 2500);
+  });
+}
 
-const trimHedges = new Promise((resolve, reject) => {
-  setTimeout((name) => {
-    if (Math.random() <= 0.8) {
-      resolve(`${name} finished trimming the hedges.`);
-    } else {
-      reject(`${name} fell asleep after weed eating the yard.`);
-    }
-  }, 1000);
-});
-
-const collectWood = new Promise((resolve, reject) => {
-  setTimeout((name) => {
-    if (Math.random() <= 0.7) {
-      resolve(`${name} finished collecting wood.`);
-    } else {
-      reject(`${name} fell asleep after trimming the hedges.`);
-    }
-  }, 2500);
-});
-
-const waterGarden = new Promise((resolve, reject) => {
-  setTimeout((name) => {
-    if (Math.random() <= 0.6) {
-      resolve(`${name} finished watering the garden.`);
-    } else {
-      reject(`${name} fell asleep after collecting wood.`);
-    }
-  }, 500);
-});
+function waterGarden(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() <= 0.72) {
+        resolve(`${name} finished watering the garden.`);
+      } else {
+        reject(`${name} fell asleep after collecting wood.`);
+      }
+    }, 500);
+  });
+}
 
 function doSummerChores(name) {
   if (typeof name != "string") {
     console.log("Entry must be a string.");
   } else {
-    mowYard
-      .then((value) => {
-        console.log(value);
-        return weedEat;
-      })
-      .then((value) => {
-        console.log(value);
-        return trimHedges;
-      })
-      .then((value) => {
-        console.log(value);
-        return collectWood;
-      })
-      .then((value) => {
-        console.log(value);
-        return waterGarden;
-      })
-      .then((value) => {
-        console.log(value);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    mowYard(name).then(value => {console.log(value); return weedEat(name)})
+             .then(value => {console.log(value); return trimHedges(name)})
+             .then(value => {console.log(value); return collectWood(name)})
+             .then(value => {console.log(value); return waterGarden(name)})
+             .then(value => {console.log(value)})
+             .then(() => console.log(`${name} has finished all the chores!`))
+             .catch(error => console.log(error));
   }
 }
 
